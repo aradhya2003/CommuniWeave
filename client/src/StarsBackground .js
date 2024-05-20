901003c91c3631f1483674fb042517962aba913a
aradhya2003 <tehariaaradhya@gmail.com>
2024-05-20 13:45:32 +0530

    Initial commit: Uploading project files

---
+++ client/src/StarsBackground .js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './StarsBackground.css'; // Import the CSS file for styling
+
+const StarsBackground = () => {
+  return (
+    <div>
+      {/* Provided HTML code */}
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="title">
+        <span>PURE CSS</span>
+        <br />
+        <span>PARALLAX PIXEL STARS</span>
+      </div>
+    </div>
+  );
+};
+
+export default StarsBackground;
